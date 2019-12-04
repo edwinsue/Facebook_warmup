10cb10c5307591c1a97c39eccdde2f82c460a019
Edwin Sue <edwinsue92@gmail.com>
2019-12-04 09:31:17 -0500

    facebook

---
+++ facebook_warmup.sql
@@ -0,0 +1,14 @@
+
+
+WITH comment_count_by_user AS 
+    (SELECT 
+    COUNT(*) AS comment_count
+    FROM users u
+    INNER JOIN user_comments uc ON u.id = uc.user_id
+    WHERE uc.created_at > '2019-01-01' AND uc.created_at < '2019-12-31'
+    GROUP BY u.id)
+
+SELECT 
+comment_count || ' comments' AS bin_val
+COUNT(*) OVER (PARTITION BY comment_count) AS frequency
+FROM comment_count_by_user
